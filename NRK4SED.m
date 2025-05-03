efd27ff5a9e25cd0fb93e3858d1e8da3ff62eecf
Nunes003 <j.m.a.nunes06@gmail.com>
2025-05-03 19:15:18 +0100

    Adição de Todos os métodos e funcionaliadade da lampada de erro

--- NRK4SED.m
+++ NRK4SED.m
@@ -1,30 +1,70 @@
-function [t,u,v] = NRK4SED(f,g,a,b,n,u0,v0)
-            h = (b-a)/n;
-            t = a:h:b;
-            u = zeros(1,n+1);
-            v = zeros(1,n+1);
-            u(1) = u0;
-            v(1) = v0;
-            for i = 1:n
-                
-                %k1
-                k1u = h*f(t(i),u(i),v(i));
-                k1v = h*g(t(i),u(i),v(i));
+function [t, u, v] = NRK4SED(f, g, a, b, n, u0, v0)
+%NRK4SED Método de Runge-Kutta de 4ª ordem para um Sistema de EDOs/PVI
+%   Resolve um sistema de duas equações diferenciais de 1ª ordem do tipo:
+%       u' = f(t, u, v)
+%       v' = g(t, u, v)
+%   com condições iniciais: u(a) = u0, v(a) = v0
+%
+%   Fórmulas de iteração:
+%     k1u = h * f(t(i), u(i), v(i))
+%     k1v = h * g(t(i), u(i), v(i))
+%
+%     k2u = h * f(t(i) + h, u(i) + k1u, v(i) + k1v)
+%     k2v = h * g(t(i) + h, u(i) + k1u, v(i) + k1v)
+%
+%     k3u = h * f(t(i) + h/2, u(i) + k2u/2, v(i) + k2v/2)
+%     k3v = h * g(t(i) + h/2, u(i) + k2u/2, v(i) + k2v/2)
+%
+%     k4u = h * f(t(i) + h, u(i) + k3u, v(i) + k3v)
+%     k4v = h * g(t(i) + h, u(i) + k3u, v(i) + k3v)
+%
+%     u(i+1) = u(i) + (k1u + 2*k2u + 2*k3u + k4u) / 6
+%     v(i+1) = v(i) + (k1v + 2*k2v + 2*k3v + k4v) / 6
+%
+% INPUT:
+%   f  - função do lado direito da equação u' = f(t, u, v)
+%   g  - função do lado direito da equação v' = g(t, u, v)
+%   a  - limite inferior do intervalo
+%   b  - limite superior do intervalo
+%   n  - número de subintervalos
+%   u0 - condição inicial para u
+%   v0 - condição inicial para v
+%
+% OUTPUT:
+%   t  - vetor dos tempos (1 x n+1)
+%   u  - vetor das soluções aproximadas de u(t)
+%   v  - vetor das soluções aproximadas de v(t)
+%
+%   28/03/2025  Afonso Mariz Luís         a2022127026@isec.pt
+%   28/03/2025  Diogo Bento Santos        a2022108969@isec.pt
+%   28/03/2025  João Manuel Almeida Nunes a2022122159@isec.pt
 
-                %k2
-                k2u = h*f(t(i+1),u(i)+k1u,v(i)+k1v);
-                k2v = h*g(t(i+1),u(i)+k1u,v(i)+k1v);   
+    h = (b - a) / n;
+    t = a:h:b;
+    u = zeros(1, n+1);
+    v = zeros(1, n+1);
+    u(1) = u0;
+    v(1) = v0;
 
-                %k3
-                k3u = h * f(t(i) + h/2, u(i) + k2u/2, v(i) + k2v/2);
-                k3v = h * g(t(i) + h/2, u(i) + k2u/2, v(i) + k2v/2);
+    for i = 1:n
+        % k1
+        k1u = h * f(t(i), u(i), v(i));
+        k1v = h * g(t(i), u(i), v(i));
 
-                %k4
-                k4u = h * f(t(i) + h, u(i) + k3u, v(i) + k3v);
-                k4v = h * g(t(i) + h, u(i) + k3u, v(i) + k3v);
+        % k2
+        k2u = h * f(t(i) + h, u(i) + k1u, v(i) + k1v);
+        k2v = h * g(t(i) + h, u(i) + k1u, v(i) + k1v);
 
+        % k3
+        k3u = h * f(t(i) + h/2, u(i) + k2u/2, v(i) + k2v/2);
+        k3v = h * g(t(i) + h/2, u(i) + k2u/2, v(i) + k2v/2);
 
-                u(i+1) = u(i) + (k1u + 2*k2u + 2*k3u + k4u)/6;
-                v(i+1) = v(i) + (k1v + 2*k2v + 2*k3v + k4v)/6;  
-            end            
-        end
+        % k4
+        k4u = h * f(t(i) + h, u(i) + k3u, v(i) + k3v);
+        k4v = h * g(t(i) + h, u(i) + k3u, v(i) + k3v);
+
+        % Atualização das soluções
+        u(i+1) = u(i) + (k1u + 2*k2u + 2*k3u + k4u)/6;
+        v(i+1) = v(i) + (k1v + 2*k2v + 2*k3v + k4v)/6;
+    end
+end
--- ODE45.m
+++ ODE45.m
@@ -1,48 +1,43 @@
-function [T, U, V] = ODE45(f, g, a, b, n, u0, v0)
-   
-   alpha = [0, 1/4, 3/8, 12/13, 1, 1/2];
-    beta = [ ...
-        0           0           0           0         0       0;
-        1/4         0           0           0         0       0;
-        3/32        9/32        0           0         0       0;
-        1932/2197  -7200/2197   7296/2197   0         0       0;
-        439/216    -8           3680/513   -845/4104  0       0;
-        -8/27       2          -3544/2565   1859/4104 -11/40  0 ];
+function [t, u, v] = ODE45(f, g, a, b, n, u0, v0)
+%ODE45 Método de resolução numérica de EDOs usando o solver embutido do MATLAB
+%   Resolve um sistema de duas equações diferenciais de 1ª ordem do tipo:
+%       u' = f(t, u, v)
+%       v' = g(t, u, v)
+%   com condições iniciais: u(a) = u0, v(a) = v0
+%
+% INPUT:
+%   f  - função do lado direito da equação u' = f(t, u, v)
+%   g  - função do lado direito da equação v' = g(t, u, v)
+%   a  - limite inferior do intervalo
+%   b  - limite superior do intervalo
+%   n  - número de subintervalos (define os pontos em tspan)
+%   u0 - condição inicial para u
+%   v0 - condição inicial para v
+%
+% OUTPUT:
+%   t  - vetor dos tempos (1 x n+1)
+%   u  - vetor das soluções aproximadas de u(t)
+%   v  - vetor das soluções aproximadas de v(t)
+%
+% NOTA:
+%   Usa o solver adaptativo ode45 do MATLAB com o vetor de estado Y = [u; v].
+%   A saída u e v são transpostas para manter a consistência (vetores linha).
+%
+%   28/03/2025  Afonso Mariz Luís         a2022127026@isec.pt
+%   28/03/2025  Diogo Bento Santos        a2022108969@isec.pt
+%   28/03/2025  João Manuel Almeida Nunes a2022122159@isec.pt
 
-    c = [16/135  0  6656/12825  28561/56430  -9/50  2/55]; 
+    % Define o sistema vetorial como uma função anónima
+    sistema = @(t, Y) [f(t, Y(1), Y(2)); g(t, Y(1), Y(2))];
 
+    % Cria vetor de tempos com n+1 pontos igualmente espaçados
+    tspan = linspace(a, b, n+1);
+    Y0 = [u0; v0];  % Condições iniciais
 
-    h = (b - a) / n;
-    T = linspace(a, b, n+1);
-    U = zeros(1, n+1);  
-    V = zeros(1, n+1);  
-    U(1) = u0;
-    V(1) = v0;
+    % Chamada ao ode45
+    [t, Y] = ode45(sistema, tspan, Y0);
 
-    for i = 1:n
-        t = T(i);
-        u = U(i);
-        v = V(i);
-
-        ku1 = f(t, u, v);
-        ku2 = f(t + alpha(2)*h, u + h*beta(2,1)*ku1, v + h*beta(2,1)*g(t, u, v));
-        ku3 = f(t + alpha(3)*h, u + h*(beta(3,1)*ku1 + beta(3,2)*ku2), ...
-                                 v + h*(beta(3,1)*g(t,u,v) + beta(3,2)*g(t,u,v)));
-        ku4 = f(t + alpha(4)*h, u + h*(beta(4,1)*ku1 + beta(4,2)*ku2 + beta(4,3)*ku3), ...
-                                 v + h*(beta(4,1)*g(t,u,v) + beta(4,2)*g(t,u,v) + beta(4,3)*g(t,u,v)));
-        ku5 = f(t + alpha(5)*h, u + h*(beta(5,1)*ku1 + beta(5,2)*ku2 + beta(5,3)*ku3 + beta(5,4)*ku4), ...
-                                 v + h*(beta(5,1)*g(t,u,v) + beta(5,2)*g(t,u,v) + beta(5,3)*g(t,u,v) + beta(5,4)*g(t,u,v)));
-        ku6 = f(t + alpha(6)*h, u + h*(beta(6,1)*ku1 + beta(6,2)*ku2 + beta(6,3)*ku3 + beta(6,4)*ku4 + beta(6,5)*ku5), ...
-                                 v + h*(beta(6,1)*g(t,u,v) + beta(6,2)*g(t,u,v) + beta(6,3)*g(t,u,v) + beta(6,4)*g(t,u,v) + beta(6,5)*g(t,u,v)));
-
-        kv1 = g(t, u, v);
-        kv2 = g(t + alpha(2)*h, u + h*beta(2,1)*v, v + h*beta(2,1)*kv1);
-        kv3 = g(t + alpha(3)*h, u + h*(beta(3,1)*v + beta(3,2)*kv2), v + h*(beta(3,1)*kv1 + beta(3,2)*kv2));
-        kv4 = g(t + alpha(4)*h, u + h*(beta(4,1)*v + beta(4,2)*kv2 + beta(4,3)*kv3), v + h*(beta(4,1)*kv1 + beta(4,2)*kv2 + beta(4,3)*kv3));
-        kv5 = g(t + alpha(5)*h, u + h*(beta(5,1)*v + beta(5,2)*kv2 + beta(5,3)*kv3 + beta(5,4)*kv4), v + h*(beta(5,1)*kv1 + beta(5,2)*kv2 + beta(5,3)*kv3 + beta(5,4)*kv4));
-        kv6 = g(t + alpha(6)*h, u + h*(beta(6,1)*v + beta(6,2)*kv2 + beta(6,3)*kv3 + beta(6,4)*kv4 + beta(6,5)*kv5), v + h*(beta(6,1)*kv1 + beta(6,2)*kv2 + beta(6,3)*kv3 + beta(6,4)*kv4 + beta(6,5)*kv5));
-
-        U(i+1) = u + h*(c(1)*ku1 + c(3)*ku3 + c(4)*ku4 + c(5)*ku5 + c(6)*ku6);
-        V(i+1) = v + h*(c(1)*kv1 + c(3)*kv3 + c(4)*kv4 + c(5)*kv5 + c(6)*kv6);
-    end
+    % Separação dos resultados
+    u = Y(:, 1).';  % transposto para vetor linha
+    v = Y(:, 2).';
 end
